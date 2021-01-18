d3714312050d204e1f2d57036510c1a88a16693c
bielb1b2 <gabrielvt.souza@Hotmail.com>
2021-01-18 16:38:23 -0300

    criptografia de senha

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -16,6 +16,9 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
+    // @ts-expect-error Aqui vai ocorrer um erro, mas estou ignorando
+    delete user.password;
+
     return response.json(user);
   } catch (error) {
     return response.status(400).json({ error: error.message });
